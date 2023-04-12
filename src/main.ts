b6933c152b9c6617c137a80fdef5d4ab17c94efd
Pringgo Radianto <pringgo.radianto@privyid.tech>
2023-04-12 10:54:56 +0700

    feat: export pdf object addon

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
 export { default as PdfViewer } from './components/pdf-viewers/PdfViewer.vue'
 export { default as PdfObject } from './components/pdf-object/PdfObject.vue'
+export { default as PdfObjectAddon } from './components/pdf-object/PdfObjectAddon.vue'
 export type { PdfObject as PdfObjectRef } from './types/ref'
