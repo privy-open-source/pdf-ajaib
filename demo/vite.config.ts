35b366b2d6136fe39670b3aab92509cd9f3c39b9
Pringgo Radianto <pringgo.radianto@privyid.tech>
2023-05-17 12:50:02 +0700

    docs(demo): improve demo docs

---
+++ demo/vite.config.ts
@@ -0,0 +1,16 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Icons from 'unplugin-icons/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: 'demo/',
+  plugins: [vue(), Icons({ compiler: 'vue3' })],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('../src', import.meta.url))
+    }
+  },
+})
